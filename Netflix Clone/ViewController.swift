fc062d4e86d5f47c5d51e04da3c73fd8effce36a
Jonas Morales <josuemodev@gmail.com>
2023-10-14 13:14:56 -0600

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jonas Josue Morales on 14/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
